dbe5fad727f228ff120130c52675068f31b6415d
BlueBinz <truebluebinz@gmail.com>
2019-01-11 08:08:43 -0600

    2 player jumpy jump

--- Unity/Assets/_Scripts/PlayerController.cs
+++ Unity/Assets/_Scripts/PlayerController.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     //universal attributes
     Rigidbody2D rb2d;
+    List<Collider2D> ObjectsTouchingFeet;
 
     //player specific attributes
     public int playerNo;
 
     //character specific attributes
-    public int jumpHeight;
-    public int moveSpeed;
+    public float jumpForce;
+    public float moveSpeed;
 
 	// Use this for initialization
 	void Start () {
         rb2d = GetComponent<Rigidbody2D>();
+        ObjectsTouchingFeet = new List<Collider2D>();
+        jumpForce *= 550;
 	}
 	
 	// Update is called once per frame
@@ -25,6 +28,20 @@ public class PlayerController : MonoBehaviour {
 
     //Physics stuff
     void FixedUpdate() {
-        Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), Input)
+        if ((Input.GetAxis("P1.Jump") > 0)&&playerNo==1&&ObjectsTouchingFeet.ToArray().Length>0) {
+            rb2d.AddForce(Vector2.up * jumpForce);
+        }
+        if ((Input.GetAxis("P2.Jump") > 0) && playerNo == 2 && ObjectsTouchingFeet.ToArray().Length > 0)
+        {
+            rb2d.AddForce(Vector2.up * jumpForce);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        ObjectsTouchingFeet.Add(collision);
+    }
+
+    void OnTriggerExit2D(Collider2D collision) {
+        ObjectsTouchingFeet.Remove(collision);
     }
 }
