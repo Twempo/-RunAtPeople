b65adadd290c0c572e10f592476977126dd669e6
Shuttlemax <42440837+Shuttlemax@users.noreply.github.com>
2019-01-14 07:38:59 -0600

    half of monevemt works

--- Unity/Assets/_Scripts/PlayerController.cs
+++ Unity/Assets/_Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     //universal attributes
     Rigidbody2D rb2d;
-    private int direction; // 1 = right, -1 = left
+    public int direction; // 1 = right, -1 = left
     List<Collider2D> ObjectsTouchingFeet;
 
     //player specific attributes
@@ -18,7 +18,6 @@ public class PlayerController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         rb2d = GetComponent<Rigidbody2D>();
-        direction = 1;
         ObjectsTouchingFeet = new List<Collider2D>();
         jumpForce *= 550;
 	}
@@ -30,7 +29,8 @@ public class PlayerController : MonoBehaviour {
 
     //Physics stuff
     void FixedUpdate() {
-        rb2d.velocity.Set(moveSpeed*direction, rb2d.velocity.y);
+        rb2d.velocity = (new Vector2(moveSpeed * direction, rb2d.velocity.y));
+
         //rb2d.GetContacts().
         if ((Input.GetAxis("P1.Jump") > 0)&&playerNo==1&&ObjectsTouchingFeet.ToArray().Length>0) {
             rb2d.AddForce(Vector2.up * jumpForce);
