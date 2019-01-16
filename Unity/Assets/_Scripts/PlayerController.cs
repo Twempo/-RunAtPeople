70746228820b90b7b7afbc50aa88d668249a611f
BlueBinz <truebluebinz@gmail.com>
2019-01-16 07:36:45 -0600

    jumping complete (hopefully)

--- Unity/Assets/_Scripts/PlayerController.cs
+++ Unity/Assets/_Scripts/PlayerController.cs
@@ -8,10 +8,9 @@ public class PlayerController : MonoBehaviour {
     public int direction; // 1 = right, -1 = left
     List<Collider2D> ObjectsTouchingFeet;
 
-    public Collider2D footCollider;
-
     //player specific attributes
     public int playerNo;
+    private float timeToJump;
 
     //character specific attributes
     public float jumpForce;
@@ -22,19 +21,31 @@ public class PlayerController : MonoBehaviour {
         rb2d = GetComponent<Rigidbody2D>();
         ObjectsTouchingFeet = new List<Collider2D>();
         jumpForce *= 732;
+        timeToJump = 0;
 	}
+
+    private void Update() {
+        if(timeToJump > 0)
+        {
+            timeToJump -= Time.deltaTime;
+        }
+    }
 
     //Physics stuff
     void FixedUpdate() {
         rb2d.velocity = (new Vector2(moveSpeed * direction, rb2d.velocity.y));
 
         //rb2d.GetContacts().
-        if ((Input.GetAxis("P1.Jump") > 0)&&playerNo==1&&ObjectsTouchingFeet.ToArray().Length>0) {
+        if ((Input.GetAxis("P1.Jump") > 0)&&playerNo==1&&ObjectsTouchingFeet.ToArray().Length>0 && timeToJump <= 0) {
+            rb2d.velocity = new Vector2(rb2d.velocity.x,0);
             rb2d.AddForce(Vector2.up * jumpForce);
+            timeToJump = .5f;
         }
-        if ((Input.GetAxis("P2.Jump") > 0) && playerNo == 2 && ObjectsTouchingFeet.ToArray().Length > 0)
+        if ((Input.GetAxis("P2.Jump") > 0) && playerNo == 2 && ObjectsTouchingFeet.ToArray().Length > 0 && timeToJump <= 0)
         {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, 0);
             rb2d.AddForce(Vector2.up * jumpForce);
+            timeToJump = .5f;
         }
         foreach (Collider2D c in ObjectsTouchingFeet)
             Debug.Log(gameObject.name + ": " + c.name); 
@@ -45,7 +56,11 @@ public class PlayerController : MonoBehaviour {
     }
 
     void OnTriggerEnter2D(Collider2D collision) {
-        ObjectsTouchingFeet.Add(collision);
+        if (timeToJump <= 0)
+        {
+            ObjectsTouchingFeet.Add(collision);
+            Debug.Log("halp");
+        }
     }
 
     void OnTriggerExit2D(Collider2D collision)
