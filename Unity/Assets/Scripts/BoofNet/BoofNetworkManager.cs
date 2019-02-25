772b6f932815f6d79319f220355bea0cb96b0b5f
RattyBloom40 <e10benga@gmail.com>
2019-02-25 08:08:33 -0600

    Starting on BoofNet API

---
+++ Unity/Assets/Scripts/BoofNet/BoofNetworkManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace BoofNet {
+    [AddComponentMenu("BoofNet/BoofNetworkManager")]
+    public class BoofNetworkManager : MonoBehaviour {
+        private static INetworkTransport s_ActiveTransport = (INetworkTransport)new DefaultNetworkTransport();
+
+    }
+}
