da99682e2a55e622bdca08d7d728029411758d03
RattyBloom40 <e10benga@gmail.com>
2019-03-25 08:12:45 -0500

    Im not sure what I did

---
+++ Unity/Assets/Scripts/TitleInator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleInator : MonoBehaviour
+{
+    public float flipSpeed;
+
+    float timeToFlip = 0;
+
+    Vector3 A = new Vector3(0, 0, 45);
+    Vector3 B = new Vector3(0, 0, -45);
+    
+    void Update()
+    {
+        timeToFlip -= Time.deltaTime;
+        if(timeToFlip<=0) {
+            timeToFlip = 1 / flipSpeed;
+            transform.rotation = Quaternion.Euler((transform.rotation.eulerAngles == A) ?  B : A);
+        }
+    }
+}
